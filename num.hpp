2c34e1b653915829d2888466d0d7d67de9b83d09
Harris Hancock <vortrab@gmail.com>
2013-04-28 13:23:20 -0700

    WIP

--- num.hpp
+++ num.hpp
@@ -3,6 +3,8 @@
 
 //////////////////////////////////////////////////////////////////////////////
 
+/* Num tag class */
+
 template <typename LHS, typename RHS>
 struct add;
 
@@ -94,6 +96,8 @@ struct not_ : bool_<!Bool::value> { };
 
 //////////////////////////////////////////////////////////////////////////////
 
+/* Eq tag class */
+
 template <typename LHS, typename RHS>
 struct eq;
 
@@ -122,6 +126,131 @@ template <typename LHS, typename RHS>
 struct neq : apply<neq_impl<typename LHS::tag>, LHS, RHS> {
     static_assert(::std::is_same<typename LHS::tag, typename RHS::tag>::value,
             "neq argument tag mismatch");
+};
+
+//////////////////////////////////////////////////////////////////////////////
+
+/* Enum tag class */
+
+template <typename T>
+struct succ;
+
+template <typename T>
+struct pred;
+
+template <typename Tag>
+struct succ_impl;
+
+template <typename Tag>
+struct pred_impl;
+
+template <typename T>
+struct succ : apply<succ_impl<typename T::tag>, T> { };
+
+template <typename T>
+struct pred : apply<pred_impl<typename T::tag>, T> { };
+
+//////////////////////////////////////////////////////////////////////////////
+
+/* Integral tag class */
+
+template <typename LHS, typename RHS>
+struct quot;
+
+template <typename LHS, typename RHS>
+struct rem;
+
+template <typename LHS, typename RHS>
+struct div;
+
+template <typename LHS, typename RHS>
+struct mod;
+
+template <typename LHS, typename RHS>
+struct quot_rem;
+
+template <typename LHS, typename RHS>
+struct div_mod;
+
+template <typename Tag>
+struct quot_impl {
+    template <typename LHS, typename RHS>
+    struct apply : fst<typename quot_rem<LHS, RHS>::type> { };
+};
+
+template <typename Tag>
+struct rem_impl {
+    template <typename LHS, typename RHS>
+    struct apply : snd<typename quot_rem<LHS, RHS>::type> { };
+};
+
+template <typename Tag>
+struct div_impl {
+    template <typename LHS, typename RHS>
+    struct apply : fst<typename div_mod<LHS, RHS>::type> { };
+};
+
+template <typename Tag>
+struct mod_impl {
+    template <typename LHS, typename RHS>
+    struct apply : snd<typename div_mod<LHS, RHS>::type> { };
+};
+
+template <typename Tag>
+struct quot_rem_impl;
+
+template <typename Tag>
+struct div_mod_impl {
+    template <typename LHS, typename RHS>
+    struct apply {
+        using qr = typename quot_rem<LHS, RHS>::type;
+        using q = typename fst<qr>::type;
+        using r = typename snd<qr>::type;
+        using type = typename ::std::conditional<
+            typename eq<
+                    typename signum<LHS>::type,
+                    typename negate<typename signum<RHS>::type>::type
+            >::type::value,
+            tuple<typename pred<q>::type, typename add<r, RHS>::type>,
+            qr
+        >::type;
+    };
+};
+
+template <typename LHS, typename RHS>
+struct quot : apply<quot_impl<typename LHS::tag>, LHS, RHS> {
+    static_assert(::std::is_same<typename LHS::tag, typename RHS::tag>::value,
+            "quot argument tag mismatch");
+};
+
+template <typename LHS, typename RHS>
+struct rem : apply<rem_impl<typename LHS::tag>, LHS, RHS> {
+    static_assert(::std::is_same<typename LHS::tag, typename RHS::tag>::value,
+            "rem argument tag mismatch");
+};
+
+template <typename LHS, typename RHS>
+struct div : apply<div_impl<typename LHS::tag>, LHS, RHS> {
+    static_assert(::std::is_same<typename LHS::tag, typename RHS::tag>::value,
+            "div argument tag mismatch");
+};
+
+template <typename LHS, typename RHS>
+struct mod : apply<mod_impl<typename LHS::tag>, LHS, RHS> {
+    static_assert(::std::is_same<typename LHS::tag, typename RHS::tag>::value,
+            "mod argument tag mismatch");
+};
+
+template <typename LHS, typename RHS>
+struct quot_rem : apply<quot_rem_impl<typename LHS::tag>, LHS, RHS> {
+    static_assert(::std::is_same<typename LHS::tag, typename RHS::tag>::value,
+            "quot_rem argument tag mismatch");
+};
+
+template <typename LHS, typename RHS>
+struct div_mod : apply<div_mod_impl<typename LHS::tag>, LHS, RHS> {
+    static_assert(::std::is_same<typename LHS::tag, typename RHS::tag>::value,
+            "div_mod argument tag mismatch");
 };
 
 //////////////////////////////////////////////////////////////////////////////
@@ -170,6 +299,43 @@ struct eq_impl<int_tag> {
     struct apply : bool_<(LHS::value == RHS::value)> { };
 };
 
+template <>
+struct succ_impl<int_tag> {
+    template <typename T>
+    struct apply : int_<T::value + 1> { };
+};
+
+template <>
+struct pred_impl<int_tag> {
+    template <typename T>
+    struct apply : int_<T::value - 1> { };
+};
+
+template <>
+struct quot_rem_impl<int_tag> {
+    template <typename LHS, typename RHS>
+    struct apply {
+        using type = tuple<
+
+    };
+};
+
 //////////////////////////////////////////////////////////////////////////////
+
+struct ratio_tag;
+
+template <typename Num, typename Den>
+struct ratio {
+    using tag = ratio_tag;
+    static_assert(::std::is_same<typename Num::tag, typename Den::tag>::value,
+            "ratio argument tag mismatch");
+    using num = typename multiply<
+        typename signum<typename multiply<Num, Den>::type>::type,
+        typename abs<Num>::type
+    >::type;
+    using den = typename abs<Den>::type;
+    using divisor = typename gcd<Num, Den>::type;
+    using type = ratio<typename divide<Num, divisor>::type, typename divide<Den, divisor>::type>;
+};
 
 #endif
