43b3b8ed507b8585c5c0aee364885619765d9fd3
Harris Hancock <vortrab@gmail.com>
2013-04-29 23:12:51 -0700

    quot_rem

--- num.hpp
+++ num.hpp
@@ -1,82 +1,29 @@
 #ifndef TYPE_CONTAINERS_NUM_HPP
 #define TYPE_CONTAINERS_NUM_HPP
 
-//////////////////////////////////////////////////////////////////////////////
+template <typename Fst, typename Snd>
+struct tuple2 {
+    using fst = Fst;
+    using snd = Snd;
+    using type = tuple2<Fst, Snd>;
+};
 
-/* Num tag class */
+template <typename Tuple>
+struct fst {
+    using type = typename Tuple::fst;
+};
 
-template <typename LHS, typename RHS>
-struct add;
-
-template <typename LHS, typename RHS>
-struct multiply;
-
-template <typename LHS, typename RHS>
-struct subtract;
-
-template <typename T>
-struct negate;
-
-template <typename T>
-struct abs;
-
-template <typename T>
-struct signum;
+template <typename Tuple>
+struct snd {
+    using type = typename Tuple::snd;
+};
 
 //////////////////////////////////////////////////////////////////////////////
 
 template <typename Tag>
-struct add_impl;
+struct value_ctor;
 
-template <typename Tag>
-struct multiply_impl;
-
-template <typename Tag>
-struct subtract_impl {
-    template <typename LHS, typename RHS>
-    struct apply : add<LHS, typename negate<RHS>::type> { };
-};
-
-template <typename Tag>
-struct negate_impl {
-    template <typename T>
-    struct apply : subtract<typename subtract<T, T>::type, T> { };
-};
-
-template <typename Tag>
-struct abs_impl;
-
-template <typename Tag>
-struct signum_impl;
-
-//////////////////////////////////////////////////////////////////////////////
-
-template <typename LHS, typename RHS>
-struct add : apply<add_impl<typename LHS::tag>, LHS, RHS> {
-    static_assert(::std::is_same<typename LHS::tag, typename RHS::tag>::value,
-            "add argument tag mismatch");
-};
-
-template <typename LHS, typename RHS>
-struct multiply : apply<multiply_impl<typename LHS::tag>, LHS, RHS> {
-    static_assert(::std::is_same<typename LHS::tag, typename RHS::tag>::value,
-            "multiply argument tag mismatch");
-};
-
-template <typename LHS, typename RHS>
-struct subtract : apply<subtract_impl<typename LHS::tag>, LHS, RHS> { };
-    static_assert(::std::is_same<typename LHS::tag, typename RHS::tag>::value,
-            "subtract argument tag mismatch");
-};
-
-template <typename T>
-struct negate : apply<negate_impl<typename T::tag>, T> { };
-
-template <typename T>
-struct abs : apply<abs_impl<typename T::tag>, T> { };
-
-template <typename T>
-struct signum : apply<signum_impl<typename T::tag>, T> { };
+#define DIGIT(TAG, D) value_ctor<TAG>::template apply<D>::type;
 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -96,167 +43,106 @@ struct not_ : bool_<!Bool::value> { };
 
 //////////////////////////////////////////////////////////////////////////////
 
-/* Eq tag class */
+/* Num tag class */
 
-template <typename LHS, typename RHS>
-struct eq;
+template <typename LHS, typename RHS, typename Tag = LHS::tag>
+struct add;
 
-template <typename LHs, typename RHS>
-struct neq;
+template <typename LHS, typename RHS, typename Tag = LHS::tag>
+struct multiply;
 
-template <typename Tag>
-struct eq_impl {
-    template <typename LHS, typename RHS>
-    struct apply : not_<typename neq<LHS, RHS>::type> { };
-};
+template <typename LHS, typename RHS, typename Tag = LHS::tag>
+struct subtract;
 
-template <typename Tag>
-struct neq_impl {
-    template <typename LHS, typename RHS>
-    struct apply : not_<typename eq<LHS, RHS>::type> { };
-};
+template <typename T, typename Tag = T::tag>
+struct negate;
 
-template <typename LHS, typename RHS>
-struct eq : apply<eq_impl<typename LHS::tag>, LHS, RHS> {
-    static_assert(::std::is_same<typename LHS::tag, typename RHS::tag>::value,
-            "eq argument tag mismatch");
-};
+template <typename T, typename Tag = T::tag>
+struct abs;
 
-template <typename LHS, typename RHS>
-struct neq : apply<neq_impl<typename LHS::tag>, LHS, RHS> {
-    static_assert(::std::is_same<typename LHS::tag, typename RHS::tag>::value,
-            "neq argument tag mismatch");
-};
+template <typename T, typename Tag = T::tag>
+struct signum;
+
+/* Default implementations */
+
+template <typename LHS, typename RHS, typename Tag>
+struct subtract : add<LHS, typename negate<RHS>::type> { };
+
+template <typename T, typename Tag>
+struct negate : subtract<typename subtract<T, T>::type, T> { };
 
 //////////////////////////////////////////////////////////////////////////////
 
-/* Enum tag class */
+/* Eq tag class */
 
-template <typename T>
-struct succ;
+template <typename LHS, typename RHS, typename Tag = LHS::tag>
+struct eq;
 
-template <typename T>
-struct pred;
+template <typename LHS, typename RHS, typename Tag = LHS::tag>
+struct neq;
 
-template <typename Tag>
-struct succ_impl;
+/* Default implementations */
 
-template <typename Tag>
-struct pred_impl;
+template <typename LHS, typename RHS, typename Tag>
+struct eq : not_<typename neq<LHS, RHS>::type> { };
 
-template <typename T>
-struct succ : apply<succ_impl<typename T::tag>, T> { };
-
-template <typename T>
-struct pred : apply<pred_impl<typename T::tag>, T> { };
+template <typename LHS, typename RHS, typename Tag>
+struct neq : not_<typename eq<LHS, RHS>::type> { };
 
 //////////////////////////////////////////////////////////////////////////////
 
 /* Integral tag class */
 
-template <typename LHS, typename RHS>
+template <typename LHS, typename RHS, typename Tag = LHS::tag>
 struct quot;
 
-template <typename LHS, typename RHS>
+template <typename LHS, typename RHS, typename Tag = LHS::tag>
 struct rem;
 
-template <typename LHS, typename RHS>
+template <typename LHS, typename RHS, typename Tag = LHS::tag>
 struct div;
 
-template <typename LHS, typename RHS>
+template <typename LHS, typename RHS, typename Tag = LHS::tag>
 struct mod;
 
-template <typename LHS, typename RHS>
+template <typename LHS, typename RHS, typename Tag = LHS::tag>
 struct quot_rem;
 
-template <typename LHS, typename RHS>
+template <typename LHS, typename RHS, typename Tag = LHS::tag>
 struct div_mod;
 
-template <typename Tag>
-struct quot_impl {
-    template <typename LHS, typename RHS>
-    struct apply : fst<typename quot_rem<LHS, RHS>::type> { };
-};
+/* Default implementations */
 
-template <typename Tag>
-struct rem_impl {
-    template <typename LHS, typename RHS>
-    struct apply : snd<typename quot_rem<LHS, RHS>::type> { };
-};
+template <typename LHS, typename RHS, typename Tag>
+struct quot : fst<typename quot_rem<LHS, RHS>::type> { };
 
-template <typename Tag>
-struct div_impl {
-    template <typename LHS, typename RHS>
-    struct apply : fst<typename div_mod<LHS, RHS>::type> { };
-};
+template <typename LHS, typename RHS, typename Tag>
+struct rem : snd<typename quot_rem<LHS, RHS>::type> { };
 
-template <typename Tag>
-struct mod_impl {
-    template <typename LHS, typename RHS>
-    struct apply : snd<typename div_mod<LHS, RHS>::type> { };
-};
+template <typename LHS, typename RHS, typename Tag>
+struct div : fst<typename div_mod<LHS, RHS>::type> { };
 
-template <typename Tag>
-struct quot_rem_impl;
+template <typename LHS, typename RHS, typename Tag>
+struct mod : snd<typename div_mod<LHS, RHS>::type> { };
 
-template <typename Tag>
-struct div_mod_impl {
-    template <typename LHS, typename RHS>
-    struct apply {
-        using qr = typename quot_rem<LHS, RHS>::type;
-        using q = typename fst<qr>::type;
-        using r = typename snd<qr>::type;
-        using type = typename ::std::conditional<
-            typename eq<
-                    typename signum<LHS>::type,
-                    typename negate<typename signum<RHS>::type>::type
-            >::type::value,
-            tuple<typename pred<q>::type, typename add<r, RHS>::type>,
-            qr
-        >::type;
-    };
-};
-
-template <typename LHS, typename RHS>
-struct quot : apply<quot_impl<typename LHS::tag>, LHS, RHS> {
-    static_assert(::std::is_same<typename LHS::tag, typename RHS::tag>::value,
-            "quot argument tag mismatch");
-};
-
-template <typename LHS, typename RHS>
-struct rem : apply<rem_impl<typename LHS::tag>, LHS, RHS> {
-    static_assert(::std::is_same<typename LHS::tag, typename RHS::tag>::value,
-            "rem argument tag mismatch");
-};
-
-template <typename LHS, typename RHS>
-struct div : apply<div_impl<typename LHS::tag>, LHS, RHS> {
-    static_assert(::std::is_same<typename LHS::tag, typename RHS::tag>::value,
-            "div argument tag mismatch");
-};
-
-template <typename LHS, typename RHS>
-struct mod : apply<mod_impl<typename LHS::tag>, LHS, RHS> {
-    static_assert(::std::is_same<typename LHS::tag, typename RHS::tag>::value,
-            "mod argument tag mismatch");
-};
-
-template <typename LHS, typename RHS>
-struct quot_rem : apply<quot_rem_impl<typename LHS::tag>, LHS, RHS> {
-    static_assert(::std::is_same<typename LHS::tag, typename RHS::tag>::value,
-            "quot_rem argument tag mismatch");
-};
-
-template <typename LHS, typename RHS>
-struct div_mod : apply<div_mod_impl<typename LHS::tag>, LHS, RHS> {
-    static_assert(::std::is_same<typename LHS::tag, typename RHS::tag>::value,
-            "div_mod argument tag mismatch");
+template <typename LHS, typename RHS, typename Tag>
+struct div_mod {
+    using qr = typename quot_rem<LHS, RHS>::type;
+    using q = typename fst<qr>::type;
+    using r = typename snd<qr>::type;
+    using type = typename ::std::conditional<
+        typename eq<
+                typename signum<LHS>::type,
+                typename negate<typename signum<RHS>::type>::type
+        >::type::value,
+        tuple<typename pred<q>::type, typename add<r, RHS>::type>,
+        qr
+    >::type;
 };
 
 //////////////////////////////////////////////////////////////////////////////
 
-template <typename Tag>
-struct value_ctor;
+/* Int */
 
 struct int_tag;
 
@@ -272,65 +158,40 @@ struct int_ : ::std::integral_constant<int, I> {
     using type = int_<I>;
 };
 
-template <>
-struct add_impl<int_tag> {
-    template <typename LHS, typename RHS>
-    struct apply : int_<LHS::value + RHS::value> { };
-};
+/* Num instance */
 
-template <>
-struct multiply_impl<int_tag> {
-    template <typename LHS, typename RHS>
-    struct apply : int_<LHS::value * RHS::value> { };
-};
+template <typename LHS, typename RHS>
+struct add<LHS, RHS, int_tag> : int_<LHS::value + RHS::value> { };
 
-template <>
-struct negate_impl<int_tag> {
-    template <typename T>
-    struct apply : int_<-T::value> { };
-};
+template <typename LHS, typename RHS>
+struct multiply<LHS, RHS, int_tag> : int_<LHS::value * RHS::value> { };
 
-template <>
-struct abs_impl<int_tag> {
-    template <typename T>
-    struct apply : int_<(T::value < 0 ? -T::value : T::value)> { };
-};
+template <typename T>
+struct negate<LHS, RHS, int_tag> : int_<-T::value> { };
 
-template <>
-struct signum_impl<int_tag> {
-    template <typename T>
-    struct apply : int_<(T::value / (T::value < 0 ? -T::value : T::value))> { };
-};
+template <typename T>
+struct abs<T, int_tag> : int_<(T::value < 0 ? -T::value : T::value)> { };
 
-template <>
-struct eq_impl<int_tag> {
-    template <typename LHS, typename RHS>
-    struct apply : bool_<(LHS::value == RHS::value)> { };
-};
+template <typename T>
+struct signum<T, int_tag> : int_<(T::value / abs<T>::value)> { };
 
-template <>
-struct succ_impl<int_tag> {
-    template <typename T>
-    struct apply : int_<T::value + 1> { };
-};
+/* Eq instance */
 
-template <>
-struct pred_impl<int_tag> {
-    template <typename T>
-    struct apply : int_<T::value - 1> { };
-};
+template <typename LHS, typename RHS>
+struct eq<LHS, RHS, int_tag> : bool_<(LHS::value == RHS::value)> { };
 
-template <>
-struct quot_rem_impl<int_tag> {
-    template <typename LHS, typename RHS>
-    struct apply {
-        using type = tuple<
+/* Integral instance */
 
-    };
+template <typename LHS, typename RHS>
+struct quot_rem<LHS, RHS, int_tag> {
+    using q = LHS::value / RHS::value;
+    using r = LHS::value % abs<RHS>::value;
+    using type = tuple<q, r>;
 };
 
 //////////////////////////////////////////////////////////////////////////////
 
+#if 0
 struct ratio_tag;
 
 template <typename Num, typename Den>
@@ -346,5 +207,6 @@ struct ratio {
     using divisor = typename gcd<Num, Den>::type;
     using type = ratio<typename divide<Num, divisor>::type, typename divide<Den, divisor>::type>;
 };
+#endif
 
 #endif
